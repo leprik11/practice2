29539b7aa8c6bd6758fceaf166372d9783fac494
lepr <bilah11@mail.ru>
2025-03-17 13:31:25 +0300

    Обработчик

--- practice2/MainWindow.xaml.cs
+++ practice2/MainWindow.xaml.cs
@@ -22,9 +22,14 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
+    WorkWindow workWindow = new WorkWindow();
     private void bLogin_Click(object sender, RoutedEventArgs e)
     {
-
+        Choised = true;
+        tbLogin.IsEnabled = true;
+        tbPass1.IsEnabled = true;
+        tbPass2.IsEnabled = false;
+        bEnter.IsEnabled = true;
     }
 
     private void bRegistration_Click(object sender, RoutedEventArgs e)
@@ -34,5 +39,53 @@ public partial class MainWindow : Window
         tbPass1.IsEnabled = true;
         tbPass2.IsEnabled = true;
         bEnter.IsEnabled = true;
+    }
+
+    private void bEnter_Click(object sender, RoutedEventArgs e)
+    {
+        if (Choised == false)
+        {
+            Registration(tbLogin.Text, tbPass1.Text, tbPass2.Text);
+        }
+        else
+        {
+            Logining(tbLogin.Text, tbPass1.Text);
+        }
+    }
+
+    private void Registration(string tbL, string tbP1, string tbP2)
+    {
+        if (tbL == "")
+        {
+            MessageBox.Show("Введите логин");
+            return;
+        }
+        if (tbP1 == tbP2)
+        {
+
+        }
+        else
+        {
+            MessageBox.Show("Пароли различны");
+            return;
+        }
+
+        workWindow.lUser.Content = "Текущий пользователь: " + tbL;
+        workWindow.Show();
+        Close();
+    }
+
+    private void Logining(string tbL, string tbP1)
+    {
+        if (tbL == "")
+        {
+            MessageBox.Show("Введите логин");
+            return;
+        }
+
+
+        workWindow.lUser.Content = "Текущий пользователь: " + tbL;
+        workWindow.Show();
+        Close();
     }
 }
